88802533b2a1d938d81c81a12d8a1512afc8ccdf
deenapoulose <deenapoulose123@gmail.com>
2021-03-23 15:00:01 +0530

    first

--- src/compoent/cart.js
+++ src/compoent/cart.js
@@ -1,59 +1,59 @@
-import React,{Component } from "react"; 
+import React,{Component,useState } from "react"; 
 
-class Cart extends Component  { 
-	constructor(props){
-		super(props)
-		this.state={
-			list:[]
-		}
+
+
 	
-	
-		
+const Cart = () =>{
+	const [rs,setrsdata] =useState([]);
+	var citems= localStorage.getItem("c");
+	var licitem= JSON.parse(citems);
+	var sum=0;
+	for(let i=0;i<licitem.length;i++){
+		sum=sum+licitem[i].price;
+
 	}
-
-async componentDidMount(){
-    
-    ///correct
-
-        var id1=this.props.match.params.id;
-      
-		
-		var str=JSON.parse(localStorage.getItem("prdt"));
-		let newData = str.filter(function (id,index) {
-			return index.toString() === id1 ;
-	   
-	   });
-	   localStorage.setItem("car", JSON.stringify(newData));
-	   this.setState({list:newData})
-	}
+	//console.log(sum)
+const cartremove=(i) =>{
+	var cartdata=JSON.parse(localStorage.getItem("c"));
+	let newcdata = cartdata.filter(function (id,index) {
+		return index !== i;
+	 });
+	 localStorage.setItem("c", JSON.stringify(newcdata));
+	 window.location.href = "/cart";
+   
+}
 	
-	
-	render() {
 		return(
             <table>
                 <thead>
                     <th>item</th>
                     <th>image</th>
                     <th>price</th>
+					<th>Action</th>
                 </thead>
                 <tbody>
 
-						{ this.state.list.map((item,index )=>{
+						{ licitem.map((item,index )=>{
 		
 							return(<tr id={index}>
 								<td>{item.name}</td>
 								<td><img className="im" src = {item.image} alt="nooo"/></td>
 								<td>{item.price}</td>
+								<td><button onClick={()=>cartremove(index)}>Delete</button></td>
 
 							</tr>)
 						})
 						}
+						<tr>
+							<td>price</td>
+							<td>{sum}</td>
+						</tr>
 						</tbody>
             </table>
         )
 			
 } 
-}
+
 
 
 export default Cart;
--- src/compoent/product.js
+++ src/compoent/product.js
@@ -7,9 +7,12 @@ import f4 from './images/img3.jpg'
 import f5 from './images/img5.jpg'
 import './product.css';
 import React,{useState} from "react"; 
+import { isElementOfType } from 'react-dom/test-utils';
 
 const Product = () =>{
 	const [prdt,setrdata] =useState([]);
+	const [c,setcartdata] =useState([]);
+
 
 		prdt.push({
 			image:f1,
@@ -42,6 +45,67 @@ const Product = () =>{
 		 localStorage.setItem("prdt", JSON.stringify(prdt));
 		 var da= localStorage.getItem("prdt");
 		 var log = JSON.parse(da);
+const addcart = (i) =>{
+	var cartdata=JSON.parse(localStorage.getItem("c"));
+	let newcdata = cartdata.filter(function (id,index) {
+		return index === i;
+	 });
+	var nl=newcdata.length;
+	console.log(nl);
+	let temp= prdt[i];
+	var cdata= localStorage.getItem("c");
+	console.log("c"+cdata);
+	
+	if(nl>0){
+		
+		alert("item alreday  in cart");
+
+	}
+	else{
+		if(cdata !=null){
+			var cre = JSON.parse(cdata);
+			cre.push(temp)
+		}
+		else {
+			c.push(temp)
+			cre=c;
+		}
+		setcartdata(cre);
+		localStorage.setItem("c", JSON.stringify(cre));
+		
+	}
+	
+	  
+	
+	// 	// else{
+	// 		if(cdata !=null){
+	// 			var cre = JSON.parse(cdata);
+	// 			cre.push(temp)
+	// 		}
+	// 		else {
+	// 			c.push(temp)
+	// 			cre=c;
+	// 		}
+	// 		setcartdata(cre);
+	// 		localStorage.setItem("c", JSON.stringify(cre))
+	// 	// }
+	//  }
+//coorect
+// 	if(cdata !=null){
+// 	   var cre = JSON.parse(cdata);
+// 	   cre.push(temp)
+//    }
+//    else {
+// 	   c.push(temp)
+// 	   cre=c;
+//    }
+//    setcartdata(cre);
+//    localStorage.setItem("c", JSON.stringify(cre));
+
+}
+	
+
+
 
 			return(
 				<div>
@@ -62,7 +126,8 @@ const Product = () =>{
 								<td>{item.name}</td>
 								<td><img className="im" src = {item.image} alt="nooo"/></td>
 								<td>{item.price}</td>
-								<td><Link to={`cart/${index}`} >View</Link></td>
+									<td><button onClick={()=>addcart(index)}>CART</button></td>
+								{/* <td><Link to={`cart/${index}`} >Add to cart</Link></td> */}
 
 							</tr>)
 						})
